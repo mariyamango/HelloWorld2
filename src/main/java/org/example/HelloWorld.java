e95183afe384d87f7f7aa42f33f9c6a3b44a6bdf
mariyamango <de.msomkova@gmail.com>
2024-08-29 15:01:34 +0200

    Initial commit

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        double a = 10.56;
+        double b = 15.7233;
+
+
+        System.out.println("Result: " + (a + b));
+    }
+}
