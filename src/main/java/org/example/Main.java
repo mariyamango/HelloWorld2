0c061f7499fd4228abe19c7fb998875c3fc9b3f1
sama-hh <samahajimuradovah@gmail.com>
2024-08-29 15:08:03 +0200

    added text

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.printf("Some text added!");
+        System.out.printf("Some text added by Sama!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
